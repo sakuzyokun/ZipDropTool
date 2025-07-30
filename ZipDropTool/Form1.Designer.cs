b54a4821686c7d3b54629d13019757d9220e91a7
sakuzyokun <sakuzyokun.sakuzyo0606@gmail.com>
2025-07-30 20:52:48 +0900

    Add files via upload

---
+++ ZipDropTool/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DragDropZipper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelDropHere = new System.Windows.Forms.Label();
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.buttonZip = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelDropHere
+            // 
+            this.labelDropHere.AutoSize = true;
+            this.labelDropHere.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.labelDropHere.Location = new System.Drawing.Point(12, 9);
+            this.labelDropHere.Name = "labelDropHere";
+            this.labelDropHere.Size = new System.Drawing.Size(357, 21);
+            this.labelDropHere.TabIndex = 0;
+            this.labelDropHere.Text = "ここにファイルをドラッグ＆ドロップしてください";
+            this.labelDropHere.Click += new System.EventHandler(this.labelDropHere_Click);
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.ItemHeight = 12;
+            this.listBoxFiles.Location = new System.Drawing.Point(16, 33);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(353, 220);
+            this.listBoxFiles.TabIndex = 1;
+            // 
+            // buttonZip
+            // 
+            this.buttonZip.Location = new System.Drawing.Point(16, 259);
+            this.buttonZip.Name = "buttonZip";
+            this.buttonZip.Size = new System.Drawing.Size(353, 29);
+            this.buttonZip.TabIndex = 2;
+            this.buttonZip.Text = "ZIPに圧縮する";
+            this.buttonZip.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(294, 230);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "削除";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 295);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonZip);
+            this.Controls.Add(this.listBoxFiles);
+            this.Controls.Add(this.labelDropHere);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ZipDropTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelDropHere;
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonZip;
+        private System.Windows.Forms.Button buttonRemove;
+    }
+}
+
---
+++ ZipDropTool/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DragDropZipper
+{
+    public partial class Form1 : Form
+    {
+        private List<string> droppedFiles = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // フォームにドラッグ＆ドロップ許可
+            this.AllowDrop = true;
+            this.DragEnter += Form1_DragEnter;
+            this.DragDrop += Form1_DragDrop;
+
+            // ボタンイベント
+            buttonZip.Click += buttonZip_Click;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            droppedFiles.AddRange(files);
+            droppedFiles = droppedFiles.Distinct().ToList(); // 重複除去
+
+            listBoxFiles.Items.Clear();
+            listBoxFiles.Items.AddRange(droppedFiles.Select(Path.GetFileName).ToArray());
+        }
+
+        private void buttonZip_Click(object sender, EventArgs e)
+        {
+            if (droppedFiles.Count == 0)
+            {
+                MessageBox.Show("まずはファイルをドロップしてね！");
+                return;
+            }
+
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "ZIPファイル (*.zip)|*.zip";
+                sfd.FileName = "CompressedFiles.zip";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (ZipArchive zip = ZipFile.Open(sfd.FileName, ZipArchiveMode.Create))
+                        {
+                            foreach (string file in droppedFiles)
+                            {
+                                if (File.Exists(file))
+                                    zip.CreateEntryFromFile(file, Path.GetFileName(file));
+                            }
+                        }
+
+                        MessageBox.Show("圧縮完了！");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("エラー: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void UpdateListBox()
+        {
+            listBoxFiles.Items.Clear();
+            foreach (var file in droppedFiles)
+            {
+                listBoxFiles.Items.Add(file);
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            var selected = listBoxFiles.SelectedItems.Cast<string>().ToList();
+            foreach (var item in selected)
+            {
+                droppedFiles.Remove(item);
+            }
+
+            UpdateListBox();
+        }
+
+        private void labelDropHere_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
